12d4fd0af5abc99c2c942c96c25ac717c20e2049
Kevin <kevinrisqi22@gmail.com>
2019-10-07 14:18:45 +0700

    Make View Pager

--- app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/SplashActivity.java
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/SplashActivity.java
@@ -21,8 +21,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(SplashActivity.this,WelcomeActivity.class));
                 finish();
             }
-        },5000);
-    }
+        },1000);
     }
 
 
